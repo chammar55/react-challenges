0d0bca8b629194e385a2e4ba5073ecc7d731d622
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2023-07-25 22:25:54 +0500

    2nd Accordion
    Using Children props and state lifting up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./AccordionV1.css";
-import AccordionV1 from "./AccordionV1.js";
+import AccordionV2 from "./AccordionV2.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AccordionV1 />
+    <AccordionV2 />
   </React.StrictMode>
 );
