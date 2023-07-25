06083d6f4a5c56ac745facedc20708599ddaa264
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2023-07-25 19:14:45 +0500

    1st Accordion

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./AccordionV1.css";
+import AccordionV1 from "./AccordionV1.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AccordionV1 />
+  </React.StrictMode>
+);
