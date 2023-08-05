c77d2abeaaf364669868bcd5d22791f66052ed48
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2023-08-05 10:45:08 +0500

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./AccordionV1.css";
-import Text_Expander from "./Text_Expander.js";
+import MyTabs from "./MyTabs.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Text_Expander />
+    <MyTabs />
   </React.StrictMode>
 );
