f4bb56bec5f3d3a044031de37bc5aa00368cf623
chammar55 <chammar5255@gmail.com>
2023-08-07 18:30:40 +0500

    .

---
+++ src/CurrencyConverter.js
@@ -0,0 +1,23 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import "./CurrencyConverter.css";
+
+export default function CurrencyConverter() {
+  return (
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./AccordionV1.css";
-import MyTabs from "./MyTabs.js";
+// import MyTabs from "./MyTabs.js";
+import CurrencyConverter from "./CurrencyConverter.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MyTabs />
+    <CurrencyConverter />
   </React.StrictMode>
 );
