20fe79b7e88ea939d74cceefb9c3432484ea2cb0
chammar55 <chammar5255@gmail.com>
2023-08-11 12:25:41 +0500

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./AccordionV1.css";
+import MakeCustomHook from "./MakeCustomHook/MakeCustomHook";
 // import MyTabs from "./MyTabs.js";
-import CurrencyConverter from "./CurrencyConverter.js";
+// import CurrencyConverter from "./CurrencyConverter.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CurrencyConverter />
+    <MakeCustomHook />
   </React.StrictMode>
 );
